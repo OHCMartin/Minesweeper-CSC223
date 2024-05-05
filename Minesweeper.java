fe06987cef78a334215ef1c77c1b2ddb8dcf5200
OHCMartin <166667187+OHCMartin@users.noreply.github.com>
2024-05-06 09:11:01 +1200

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,33 +1,81 @@
-
 /**
- * Write a description of class Minesweeper here.
+ * Write a description of class Minesweeper10523 here.
  *
  * @author Oscar Martin
- * @version V1 19/03/2024
+ * @version 2/05/24
  */
 import java.util.Random;
-public class Minesweeper
-{
-    private int x;
+import java.util.Scanner;
+public class Minesweeper20524 {
+    Scanner keyboard;
     /**
-     * Constructor for objects of class Minesweeper
+     * Constructor for objects of class Minesweeper10523
      */
-    public Minesweeper()
-    {
+    public Minesweeper20524() {
         Random rand = new Random();
-        int number= rand.nextInt(10);
+        int number = rand.nextInt(10);
         System.out.println(number);
-        x = 0;
-    }
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println(keyboard.nextInt());
+        boolean[][] mineField = new boolean[10][10];
+        boolean[][] mine = new boolean[10][10];
+         boolean gameOver = false;
+        int row = 9;
+        int column = 0;
+        int userPoints = 0;
+        Scanner myObj = new Scanner(System.in);
+          for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                mine[i][j] = false;
+            }
+        }
+         mine[4][2] = true;
+        for(int i=0; i<10; i++){
+            System.out.println();
+            for(int j=0; j<10; j++){
+                System.out.print(" ~ ");
+            }
+        }
+        while(gameOver == false){
+            
+            System.out.println();
+            
+            System.out.println("What row do you want to search?");
+            row = myObj.nextInt();
+            
+            System.out.println("What column do you want to search?");
+            column = myObj.nextInt();
+            
+            mineField[row][column] = true;
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        return x + y;
+            
+            updategarden(mineField, mine);
+            
+            if(mine[row][column]){
+                userPoints +=1;
+            }
+            if(userPoints == 1){
+                System.out.println("You found the carrot!");
+                gameOver = true;
+            }
+            System.out.println();
+        }
+    }
+    public static void updategarden(boolean[][] upgarden, boolean[][]carrot){
+        System.out.print('\u000C');
+        for(int i=0; i<10; i++){
+            System.out.println();
+            for(int j=0;j<10; j++){
+                if(upgarden[i][j] == true && mine[i][j] == true){
+                    System.out.print(" X ");
+                }
+                else if(upgarden[i][j] == true && mine [i][j] == false){
+                    System.out.print(" O ");
+                }
+                else{
+                    System.out.print(" ~ ");
+                }
+                }
+        }
     }
 }
